0995f283cbc727844b5e15f57d84e1f0eed6b115
Patrick Lorentzen <patrick.lorentzen@outlook.com>
2020-05-03 19:32:28 +0200

    Patrick Test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+String test = "Fra Patrick";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
