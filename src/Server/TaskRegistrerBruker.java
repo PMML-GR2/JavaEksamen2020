67fa60bab18c5acff2809cba54fbe1660d6eef4a
Patrick Lorentzen <patrick.lorentzen@outlook.com>
2020-05-04 19:34:18 +0200

    Pusset på funksjoner

--- src/Server/TaskRegistrerBruker.java
+++ src/Server/TaskRegistrerBruker.java
@@ -1,6 +1,7 @@
 package Server;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,6 +19,7 @@ public class TaskRegistrerBruker implements Runnable{
     //ServerVariabler
     Socket socket;
     DataInputStream innTekst = null;
+    DataOutputStream utTekst = null;
 
     public TaskRegistrerBruker(Socket socket){
         this.socket = socket;
@@ -32,6 +34,7 @@ public class TaskRegistrerBruker implements Runnable{
 
                 //Lag dataOutput til klient
                 innTekst = new DataInputStream(socket.getInputStream());
+                utTekst = new DataOutputStream(socket.getOutputStream());
 
                 //Skriv til klienten
                 navn = innTekst.readUTF();
@@ -48,6 +51,8 @@ public class TaskRegistrerBruker implements Runnable{
                 }
 
                 DatingDB.insertData(navn,k,alder,interesser,bosted,tlf);
+
+                utTekst.writeInt(101);
                 System.out.println("En bruker er registrert");
 
 
@@ -56,6 +61,7 @@ public class TaskRegistrerBruker implements Runnable{
         } finally {
             try {
                 innTekst.close();
+                utTekst.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
