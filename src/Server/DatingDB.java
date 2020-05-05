69ab1e31031daf98b35b98bdbc139a3c12546dbf
Mikael Rønnevik <mikaelron@gmail.com>
2020-05-05 13:55:02 +0200

    Database metoder

--- src/Server/DatingDB.java
+++ src/Server/DatingDB.java
@@ -17,11 +17,9 @@ public class DatingDB {
         return conn;
     }
    static public void registrerBruker(String navn, String kjonn, int alder, ArrayList<String> interesser, String bosted, String tlf) {
-       //System.out.println(navn + k + alder + bosted + tlf);
-       //for(String s: interesser) {
-       //    System.out.println(s);
-       //}
-       String sql = "INSERT INTO bruker(Navn, Kjonn, Alder, Interesser, Bosted, Tlf) VALUES (?,?,?,?,?,?)";
+       String sql = "INSERT INTO bruker(Navn, Kjonn, Alder, Interesser, Bosted, Tlf) " +
+                    "VALUES (?,?,?,?,?,?)";
+
        try (Connection conn = connect();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
            pstmt.setString(1, navn);
@@ -32,6 +30,71 @@ public class DatingDB {
            pstmt.setString(6, tlf);
            pstmt.executeUpdate();
        } catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }
+   }
+
+   static public void visNavnOgTlf(int PersonID1, int PersonID2) {
+       String sql = "SELECT Navn, Tlf FROM bruker WHERE PersonID = " + PersonID2;
+
+       try (Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+           while (rs.next()) {
+               System.out.println(rs.getString("Navn") + "\t" +
+                       rs.getString("Tlf"));
+           }
+       } catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }
+       String sql2 = "INSERT INTO interessert VALUES (?,?)";
+
+       try (Connection conn = connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql2)) {
+           pstmt.setInt(1, PersonID1);
+           pstmt.setInt(2, PersonID2);
+           pstmt.executeUpdate();
+       }
+       catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }
+   }
+
+   static public void visMineUtsendtInfo(int PersonID) {
+        ArrayList<String> bedtomNavn = new ArrayList<String>();
+        String sql = "SELECT bruker_PersonID FROM interessert WHERE likerID = " + PersonID;
+
+       try (Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+           while (rs.next()) {
+               bedtomNavn.add(rs.getString("Bruker_PersonID"));
+           }
+           //System.out.println(bedtomNavn.toString());
+       }
+       catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }
+
+       String sql2 = "SELECT PersonID FROM bruker WHERE PersonID = ";
+        int i = 0;
+               for(String navn : bedtomNavn) {
+                    if (i>= 1)
+                        sql2 += " OR PersonID = " + navn;
+                    else
+                        sql2 += navn;
+                   i++;
+               }
+               //System.out.println(sql2);
+
+       try (Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql2)) {
+           while (rs.next()) {
+               System.out.println(rs.getString("PersonID"));
+           }
+       }
+       catch (SQLException e) {
            System.out.println(e.getMessage());
        }
    }
@@ -53,4 +116,5 @@ public class DatingDB {
     static public void  selectTableWhere(int personID, char k, ArrayList alder){
        System.out.println("Fant en " + k + " i alder fra " + alder.get(0)+ " - " + alder.get(1)+ " til person " + personID);
     }
+
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -27,6 +27,7 @@ public class Server {
         TaskRegistrerBruker regBruker;
 
         //Lytter og venter på at noen skal koble seg til å lage ny bruker
+
         try{
             while(true) {
                 Socket socket = serverSocket.accept();
@@ -66,6 +67,7 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return serverSocket;
     }
 }
